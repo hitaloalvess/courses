9ccb9ac4ffb6d4ab3d12acc875d0086ea23203ee
hitaloalvess <hitalo.ralves@outlook.com>
2023-11-03 20:43:09 -0300

    feat: create structure

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { app } from './app'
+import { env } from './env'
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: 3333,
+  })
+  .then(() => console.log(`Server is Running in PORT:${env.PORT}`))
