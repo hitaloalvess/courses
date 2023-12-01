e4731668a22f78e9c4577d3585f573c01ac307cb
hitaloalvess <hitalo.ralves@outlook.com>
2023-12-01 17:57:32 -0300

    feat: add error handling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
 import Fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = Fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV === 'production') {
+    console.error(error)
+  } else {
+    // TODO: enviar mensagem a um sistema interno de monitoramento DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})
--- src/http/controllers/register-user.ts
+++ src/http/controllers/register-user.ts
@@ -1,7 +1,8 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 import { RegisterUseCase } from '../use-cases/register-user'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { PrismaUsersRepository } from '@/repositories/prisma/users-repository'
+import { UserAlreadyExistsError } from '../use-cases/errors/user-already-exists-error'
 
 export async function registerUserController(
   request: FastifyRequest,
@@ -16,13 +17,16 @@ export async function registerUserController(
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    //
     const usersRepository = new PrismaUsersRepository()
     const registerUserUseCase = new RegisterUseCase(usersRepository)
 
     await registerUserUseCase.execute({ name, email, password })
-  } catch {
-    return reply.status(409).send()
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
