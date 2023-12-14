57ca821695e9f484bbf5d14864854e6bb87033e5
hitaloalvess <hitalo.ralves@outlook.com>
2023-12-14 20:56:29 -0300

    feat: create gym use case

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,15 +1,32 @@
-import { Gym } from '@prisma/client'
+import { Gym, Prisma } from '@prisma/client'
 import { IGymsRepository } from '../interfaces/gyms-repository-interface'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryGymsRepository implements IGymsRepository {
-  public checkIns: Gym[] = []
+  public gyms: Gym[] = []
 
   async findById(id: string) {
-    const gym = this.checkIns.find((gym) => gym.id === id)
+    const gym = this.gyms.find((gym) => gym.id === id)
 
     if (!gym) {
       return null
     }
+
+    return gym
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = {
+      id: data?.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+      created_at: new Date(),
+    }
+
+    this.gyms.push(gym)
 
     return gym
   }
--- src/repositories/interfaces/gyms-repository-interface.ts
+++ src/repositories/interfaces/gyms-repository-interface.ts
@@ -1,5 +1,6 @@
-import { Gym } from '@prisma/client'
+import { Gym, Prisma } from '@prisma/client'
 
 export interface IGymsRepository {
   findById(id: string): Promise<Gym | null>
+  create(gymParams: Prisma.GymCreateInput): Promise<Gym>
 }
