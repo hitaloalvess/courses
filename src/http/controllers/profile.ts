e49df8fdac59d707e75618b600c0baaab43a5c76
hitaloalvess <hitalo.ralves@outlook.com>
2023-12-19 20:31:47 -0300

    feat: create route to get profile

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,20 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { makeGetUserProfileUseCase } from '../use-cases/factories/make-get-user-profile-use-case'
 
-export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+export async function profileController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const getUserProfile = makeGetUserProfileUseCase()
 
-  return reply.status(200).send()
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
