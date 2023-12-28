0bf1574cc357b719981c9838ee65cb2e3d29158e
hitaloalvess <hitalo.ralves@outlook.com>
2023-12-28 14:44:16 -0300

    asdas

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from 'vitest';
+import { AnswerQuestionUseCase } from "./answer-question";
+
+test('Create answer question', () => {
+
+  const answer = new AnswerQuestionUseCase().execute({
+    instructorId: '1',
+    questionId: '1',
+    content: 'Nova resposta'
+  });
+
+  expect(answer.content).toEqual('Nova resposta');
+})
