0c0ffaafcb856e3bd52adc1f93e8d5a2790c4884
hitaloalvess <hitalo.ralves@outlook.com>
2023-12-28 15:01:01 -0300

    feat: answer question use case

---
+++ answer-question.ts
@@ -0,0 +1,15 @@
+import { Answer } from "./src/domain/entities/answer";
+
+interface AnswerQuestionUseCaseRequest{
+  instructorId: string;
+  questionId: string;
+  content: string;
+}
+
+export class AnswerQuestionUseCase{
+  execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest){
+    const answer = new Answer(content);
+
+    return answer
+  }
+}
