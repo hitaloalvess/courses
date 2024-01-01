b1f81d6210f0d90e92a9fecb0225fff1462eda40
hitaloalvess <hitalo.ralves@outlook.com>
2024-01-01 19:49:36 -0300

    feat: delete question use case

---
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -0,0 +1,47 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { DeleteQuestionUseCase } from './delete-question'
+import { makeQuestion } from 'test/factories/make-question'
+import { Slug } from '../../enterprise/entities/value-objects/slug'
+import { UniqueEntityID } from '@/core/entities/value-objects/unique-entity-id'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: DeleteQuestionUseCase
+
+describe('Delete question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('it should be able to delete a question', async () => {
+    const question = makeQuestion(
+      {
+        slug: Slug.create('nova-pergunta'),
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('question-1'),
+    )
+
+    await inMemoryQuestionsRepository.create(question)
+
+    await sut.execute({ authorId: 'author-1', questionId: 'question-1' })
+
+    expect(inMemoryQuestionsRepository.questions).toHaveLength(0)
+  })
+
+  it('it should not be able to delete a question from another user', async () => {
+    const question = makeQuestion(
+      {
+        slug: Slug.create('nova-pergunta'),
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('question-1'),
+    )
+
+    await inMemoryQuestionsRepository.create(question)
+
+    await expect(() =>
+      sut.execute({ authorId: 'author-2', questionId: 'question-1' }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -4,6 +4,16 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public questions: Question[] = []
 
+  async findById(questionId: string) {
+    const question = this.questions.find(
+      (item) => item.id.toString() === questionId,
+    )
+
+    if (!question) return null
+
+    return question
+  }
+
   async findBySlug(slug: string) {
     const question = this.questions.find((item) => item.slug.value === slug)
 
@@ -16,5 +26,13 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async create(question: Question) {
     this.questions.push(question)
+  }
+
+  async delete(question: Question): Promise<void> {
+    const questionIndex = this.questions.findIndex(
+      (item) => item.id === question.id,
+    )
+
+    this.questions.splice(questionIndex, 1)
   }
 }
