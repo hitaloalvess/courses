fd56c541775a314fed243a43bb166a8210038ad4
hitaloalvess <hitalo.ralves@outlook.com>
2024-01-02 19:24:52 -0300

    feat: fetch recent questions

---
+++ src/domain/forum/application/use-cases/fetch-recent-questions.ts
@@ -0,0 +1,22 @@
+import { Question } from '../../enterprise/entities/question'
+import { QuestionsRepository } from '../repositories/questions-repository'
+
+interface FetchRecentQuestionsUseCaseRequest {
+  page: number
+}
+
+interface FetchRecentQuestionsUseCaseResponse {
+  questions: Question[]
+}
+
+export class FetchRecentQuestionsUseCase {
+  constructor(private readonly questionsRepo: QuestionsRepository) {}
+
+  async execute({
+    page,
+  }: FetchRecentQuestionsUseCaseRequest): Promise<FetchRecentQuestionsUseCaseResponse> {
+    const questions = await this.questionsRepo.findManyRecent({ page })
+
+    return { questions }
+  }
+}
