dbf655c06a5acad5f13581185e7e7ca4902c6683
hitaloalvess <hitalo.ralves@outlook.com>
2024-01-03 19:37:47 -0300

    feat: fetch question answers use case

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -0,0 +1,26 @@
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
+
+interface FetchQuestionAnswersUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionAnswersUseCaseResponse {
+  answers: Answer[]
+}
+
+export class FetchQuestionAnswersUseCase {
+  constructor(private readonly answersRepo: AnswersRepository) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionAnswersUseCaseRequest): Promise<FetchQuestionAnswersUseCaseResponse> {
+    const answers = await this.answersRepo.findManyByQuestionId(questionId, {
+      page,
+    })
+
+    return { answers }
+  }
+}
