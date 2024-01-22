f5683d432995c35504746f0157192e8a1a83915f
hitaloalvess <hitalo.ralves@outlook.com>
2024-01-22 20:00:20 -0300

    feat: authenticate controller

---
+++ src/controllers/authenticate.controller.ts
@@ -0,0 +1,52 @@
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcryptjs';
+import { ZodValidationPipe } from 'src/pipes/zod.validation.pipe';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { z } from 'zod';
+
+const authenticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+const authenticateValidationPipe = new ZodValidationPipe(
+  authenticateBodySchema,
+);
+
+type authenticateBodySchema = z.infer<typeof authenticateBodySchema>;
+
+@Controller('/sessions')
+export class AuthenticateController {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
+
+  @Post()
+  async handle(@Body(authenticateValidationPipe) body: authenticateBodySchema) {
+    const { email, password } = body;
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!userWithSameEmail) {
+      throw new UnauthorizedException('User credentials do not match');
+    }
+
+    const isPasswordValid = await compare(password, userWithSameEmail.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('User credentials do not match');
+    }
+
+    const token = this.jwt.sign({ sub: userWithSameEmail.id });
+
+    return {
+      token,
+    };
+  }
+}
--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -8,7 +8,7 @@ import {
 import { hash } from 'bcryptjs';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { z } from 'zod';
-import { ZodValidationPipe } from './pipes/zod.validation.pipe';
+import { ZodValidationPipe } from '../pipes/zod.validation.pipe';
 
 const createAccountBodySchema = z.object({
   name: z.string(),
