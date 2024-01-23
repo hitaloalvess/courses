7a7327acd64091de3e55c1fd7f97601cc9940985
hitaloalvess <hitalo.ralves@outlook.com>
2024-01-23 20:31:30 -0300

    feat: create question controller

--- src/auth/decorators/current-use.decorator.ts
+++ src/auth/decorators/current-use.decorator.ts
@@ -1,10 +1,10 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
-import { TokenSchema } from '../jwt-strategy';
+import { UserPayload } from '../jwt-strategy';
 
 export const CurrentUser = createParamDecorator(
   (_: never, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
 
-    return request.user as TokenSchema;
+    return request.user as UserPayload;
   },
 );
