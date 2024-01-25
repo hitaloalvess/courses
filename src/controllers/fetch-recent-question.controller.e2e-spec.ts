33a34afc345a8696babf25912c118864e9323064
hitaloalvess <hitalo.ralves@outlook.com>
2024-01-25 20:59:31 -0300

    test: create test e2e for fetch-recent-question

---
+++ src/controllers/fetch-recent-question.controller.e2e-spec.ts
@@ -0,0 +1,71 @@
+import { AppModule } from '@/app.module';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+
+describe('Fetch recent questions (E2E)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    await app.init();
+  });
+
+  test('[GET] /questions', async () => {
+    await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const {
+      body: { token },
+    } = await request(app.getHttpServer()).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    await request(app.getHttpServer())
+      .post('/questions')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Question 1',
+        content: 'Question content 1',
+      });
+
+    await request(app.getHttpServer())
+      .post('/questions')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Question 2',
+        content: 'Question content 2',
+      });
+
+    await request(app.getHttpServer())
+      .post('/questions')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Question 3',
+        content: 'Question content 3',
+      });
+
+    const response = await request(app.getHttpServer())
+      .get('/questions')
+      .set('Authorization', `Bearer ${token}`)
+      .send();
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({
+      questions: [
+        expect.objectContaining({ title: 'Question 3' }),
+        expect.objectContaining({ title: 'Question 2' }),
+        expect.objectContaining({ title: 'Question 1' }),
+      ],
+    });
+  });
+});
