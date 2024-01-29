cbde414cf81f22810c84b8358c9298af942c3100
hitaloalvess <hitalo.ralves@outlook.com>
2024-01-29 20:46:01 -0300

    refactor: inject dependency create question use case in create question controller

--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -2,10 +2,10 @@ import { CurrentUser } from '@/infra/auth/decorators/current-use.decorator';
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard';
 import { UserPayload } from '@/infra/auth/jwt-strategy';
 import { ZodValidationPipe } from '@/infra/pipes/zod.validation.pipe';
-import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 
 import { z } from 'zod';
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question';
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
@@ -19,7 +19,7 @@ type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor(private prisma: PrismaService) {}
+  constructor(private createQuestion: CreateQuestionUseCase) {}
 
   @Post()
   async handle(
@@ -29,15 +29,11 @@ export class CreateQuestionController {
     const { title, content } = body;
     const userId = user.sub;
 
-    const slug = this.convertToSlug(title);
-
-    await this.prisma.question.create({
-      data: {
-        authorId: userId,
-        title,
-        content,
-        slug,
-      },
+    await this.createQuestion.execute({
+      title,
+      content,
+      authorId: userId,
+      attachmentIds: [],
     });
   }
 
