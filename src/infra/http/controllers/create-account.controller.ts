e3f591e15dcacad95f37da9dd3b687c26ff8720f
hitaloalvess <hitalo.ralves@outlook.com>
2024-01-31 19:23:48 -0300

    refactor: create account for integration with create account use case and apply error handler

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -1,62 +1,33 @@
-import {
-  Body,
-  ConflictException,
-  Controller,
-  HttpCode,
-  Post,
-} from '@nestjs/common';
-import { hash } from 'bcryptjs';
+import { Body, Controller, HttpCode, Post, UsePipes } from '@nestjs/common';
 import { z } from 'zod';
-import { ZodValidationPipe } from '../../pipes/zod.validation.pipe';
-import { PrismaService } from '@/infra/database/prisma/prisma.service';
-import { Public } from '@/infra/auth/decorators/public.decorator';
+import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student';
+import { ZodValidationPipe } from '@/infra/pipes/zod.validation.pipe';
 
 const createAccountBodySchema = z.object({
   name: z.string(),
   email: z.string().email(),
   password: z.string(),
 });
-
-const createAccountValidationPipe = new ZodValidationPipe(
-  createAccountBodySchema,
-);
-
 type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
 
 @Controller('/accounts')
-@Public()
 export class CreateAccountController {
-  constructor(private prisma: PrismaService) {}
+  constructor(private registerStudent: RegisterStudentUseCase) {}
 
   @Post()
   @HttpCode(201)
-  async handle(
-    @Body(createAccountValidationPipe) body: CreateAccountBodySchema,
-  ) {
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body;
 
-    const userWithSameEmail = await this.prisma.user.findUnique({
-      where: {
-        email,
-      },
+    const result = await this.registerStudent.execute({
+      name,
+      email,
+      password,
     });
 
-    if (userWithSameEmail) {
-      throw new ConflictException(
-        'User with same e-mail address already exists.',
-      );
+    if (result.isLeft()) {
+      throw new Error();
     }
-
-    const hashedPassword = await hash(password, 8);
-
-    const accountCreated = await this.prisma.user.create({
-      data: {
-        name,
-        email,
-        password: hashedPassword,
-      },
-    });
-
-    return { account: accountCreated };
   }
 }
