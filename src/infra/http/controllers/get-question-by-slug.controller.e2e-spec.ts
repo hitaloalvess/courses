d1713c1f6768b4edf238cf3e3489277c18f998b6
hitaloalvess <hitalo.ralves@outlook.com>
2024-02-02 20:55:03 -0300

    test: refactor get-question-by-slug e2e for simplify criation of student and question in database with makePrismaStudent and token jwt

--- src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
+++ src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -1,47 +1,47 @@
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug';
 import { AppModule } from '@/infra/app.module';
+import { DatabaseModule } from '@/infra/database/database.module';
 import { INestApplication } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { Test } from '@nestjs/testing';
 import request from 'supertest';
+import { QuestionFactory } from 'test/factories/make-question';
+import { StudentFactory } from 'test/factories/make-student';
 
 describe('Get question by slug (E2E)', () => {
   let app: INestApplication;
+  let studentFactory: StudentFactory;
+  let questionFactory: QuestionFactory;
+  let jwt: JwtService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
     }).compile();
 
     app = moduleRef.createNestApplication();
+
+    studentFactory = moduleRef.get(StudentFactory);
+    questionFactory = moduleRef.get(QuestionFactory);
+    jwt = moduleRef.get(JwtService);
 
     await app.init();
   });
 
   test('[GET] /questions/:slug', async () => {
-    await request(app.getHttpServer()).post('/accounts').send({
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      password: '123456',
-    });
+    const user = await studentFactory.makePrismaStudent();
 
-    const {
-      body: { token },
-    } = await request(app.getHttpServer()).post('/sessions').send({
-      email: 'johndoe@example.com',
-      password: '123456',
-    });
+    const token = jwt.sign({ sub: user.id.toString() });
 
-    const {
-      body: { question },
-    } = await request(app.getHttpServer())
-      .post('/questions')
-      .set('Authorization', `Bearer ${token}`)
-      .send({
-        title: 'New question',
-        content: 'Question content',
-      });
+    await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+      title: 'Question 01',
+      slug: Slug.create('question-01'),
+    });
 
     const response = await request(app.getHttpServer())
-      .get(`/questions/${question.slug}`)
+      .get(`/questions/question-01`)
       .set('Authorization', `Bearer ${token}`)
       .send();
 
