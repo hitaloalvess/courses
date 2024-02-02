6ca9e2236eabb76b10d987071b56d1b915ab5642
hitaloalvess <hitalo.ralves@outlook.com>
2024-02-02 20:53:39 -0300

    test: refactor authenticate for simplify criation of student in database with makePrismaStudent

--- src/infra/http/controllers/authenticate.controller.e2e-spec.ts
+++ src/infra/http/controllers/authenticate.controller.e2e-spec.ts
@@ -1,33 +1,41 @@
 import { AppModule } from '@/infra/app.module';
+import { DatabaseModule } from '@/infra/database/database.module';
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { hash } from 'bcryptjs';
 import request from 'supertest';
+import { StudentFactory } from 'test/factories/make-student';
 
 describe('Create account (e2e)', () => {
   let app: INestApplication;
+  let studentFactory: StudentFactory;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory],
     }).compile();
 
     app = moduleRef.createNestApplication();
+    studentFactory = moduleRef.get(StudentFactory);
+
     await app.init();
   });
 
   test('[POST] /sessions', async () => {
-    await request(app.getHttpServer()).post('/accounts').send({
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      password: '123456',
+    await studentFactory.makePrismaStudent({
+      email: 'johndoe@email.com',
+      password: await hash('1234567', 8),
     });
 
     const authResponse = await request(app.getHttpServer())
       .post('/sessions')
       .send({
-        email: 'johndoe@example.com',
-        password: '123456',
+        email: 'johndoe@email.com',
+        password: '1234567',
       });
+
+    console.log(authResponse.error);
 
     expect(authResponse.statusCode).toBe(201);
     expect(authResponse.body).toEqual({
