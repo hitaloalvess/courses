5810567fb47f3cd86d4bacf0855a3c080a4ed70a
hitaloalvess <hitalo.ralves@outlook.com>
2024-02-04 20:07:08 -0300

    feat: create choose question best answer controller

--- src/domain/forum/application/use-cases/choose-question-best-answer.ts
+++ src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -4,6 +4,7 @@ import { AnswersRepository } from '../repositories/answers-repository';
 import { QuestionsRepository } from '../repositories/questions-repository';
 import { ResourceNotFoundError } from '../../../../core/errors/resource-not-found-error';
 import { NotAllowedError } from '@/core/errors/not-allowed-error';
+import { Injectable } from '@nestjs/common';
 
 interface ChooseQuestionBestAnswerRequest {
   authorId: string;
@@ -15,6 +16,7 @@ type ChooseQuestionBestAnswerResponse = Either<
   { question: Question }
 >;
 
+@Injectable()
 export class ChooseQuestionBestAnswerUseCase {
   constructor(
     private readonly answerRepo: AnswersRepository,
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -23,6 +23,8 @@ import { DeleteAnswerUseCase } from '@/domain/forum/application/use-cases/delete
 import { DeleteAnswerController } from './controllers/delete-answer.controller';
 import { FetchQuestionAnswersUseCase } from '@/domain/forum/application/use-cases/fetch-question-answers';
 import { FetchQuestionAnswersController } from './controllers/fetch-question-answers.controller';
+import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-cases/choose-question-best-answer';
+import { ChooseQuestionBestAnswerController } from './controllers/choose-question-best-answer';
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -38,6 +40,7 @@ import { FetchQuestionAnswersController } from './controllers/fetch-question-ans
     EditAnswerUseCase,
     DeleteAnswerUseCase,
     FetchQuestionAnswersUseCase,
+    ChooseQuestionBestAnswerUseCase,
   ],
   controllers: [
     CreateAccountController,
@@ -51,6 +54,7 @@ import { FetchQuestionAnswersController } from './controllers/fetch-question-ans
     EditAnswerController,
     DeleteAnswerController,
     FetchQuestionAnswersController,
+    ChooseQuestionBestAnswerController,
   ],
 })
 export class HttpModule {}
